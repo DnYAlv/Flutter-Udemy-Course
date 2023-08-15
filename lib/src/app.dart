f9a4861d8be34f9683540e6f66e7e899647a38b1
DnYAlv <dennyginting40@gmail.com>
2023-08-15 16:47:12 +0700

    Image Picture Application - First Project

---
+++ lib/src/app.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  createState(){
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    final response = await get(Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() => images.add(imageModel));
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Let\'s see some images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: fetchImage,
+          child: const Icon(Icons.add),
+        ),
+        body: ImageList(images),
+      ),
+    );
+  }
+}
