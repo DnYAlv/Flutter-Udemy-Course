49aa98b9bf2dabdda1498feb27a639d9e12f578c
DnYAlv <dennyginting40@gmail.com>
2023-08-24 10:56:23 +0700

    Login Screen with Scoped Instances (Previous Commit is Global Instance)

--- login_bloc/lib/src/app.dart
+++ login_bloc/lib/src/app.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context){
-    return const MaterialApp(
-      title: 'Log Me In!',
-      home: Scaffold (
-        body: SafeArea(
-          child: LoginScreen(),
+    return Provider(
+      child: const MaterialApp(
+        title: 'Log Me In!',
+        home: Scaffold (
+          body: SafeArea(
+            child: LoginScreen(),
+          ),
         ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
