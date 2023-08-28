dc196981f710ad6e6f127747e4f8e7e777a65bee
DnYAlv <dennyginting40@gmail.com>
2023-08-28 14:18:10 +0700

    completed fetching API and caching

--- news/lib/src/models/item_model.dart
+++ news/lib/src/models/item_model.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 class ItemModel {
   late final int? id;
   late final bool? deleted;
   late final String? type;
+  late final String? by;
   late final int? time;
   late final String? text;
   late final bool? dead;
@@ -25,4 +28,35 @@ class ItemModel {
       score = parsedJson['score'],
       title = parsedJson['title'],
       descendants = parsedJson['descendants'];
+  
+  ItemModel.fromDb(Map<String, dynamic> parsedJson)
+    : id = parsedJson['id'],
+      deleted = parsedJson['deleted'] == 1,
+      type = parsedJson['type'],
+      time = parsedJson['time'],
+      text = parsedJson['text'],
+      dead = parsedJson['dead'] == 1,
+      parent = parsedJson['parent'],
+      kids = jsonDecode(parsedJson['kids']),
+      url = parsedJson['url'],
+      score = parsedJson['score'],
+      title = parsedJson['title'],
+      descendants = parsedJson['descendants'];
+  
+  Map<String, dynamic> toMap() {
+    return <String, dynamic> {
+      "id": id,
+      "type": type,
+      "by": by,
+      "time": time,
+      "parent": parent,
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants,
+      "dead": dead! ? 1 : 0,
+      "deleted": deleted! ? 1 :0,
+      "kids": jsonEncode(kids),
+    };
+  }
 }
---
+++ news/lib/src/resources/repository.dart
@@ -0,0 +1,47 @@
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+import 'dart:async';
+
+class Repository {
+  List<Source> sources = <Source> [
+    newsDbProvider,
+    NewsApiProvider(),
+  ];
+
+  List<Cache> caches = <Cache> [
+    newsDbProvider,
+  ];
+
+  Future<List<int>>? fetchTopIds() {
+    return sources[1].fetchTopIds();
+  }
+
+  Future<ItemModel?> fetchItem(int id) async {
+    ItemModel? item;
+    Source source;
+    ItemModel emptyItem = {} as ItemModel;
+
+    for (source in sources) {
+      item = await source.fetchItem(id);
+      if (item != null) {
+        break;
+      }
+    }
+
+    for (var cache in caches) {
+      cache.addItem(item ?? emptyItem);
+    }
+
+    return item;
+  }
+}
+
+abstract class Source {
+  Future<List<int>>? fetchTopIds();
+  Future<ItemModel?> fetchItem(int id);
+}
+
+abstract class Cache {
+  Future<int> addItem(ItemModel item);
+}
